8a487dcc56fb6dbaba1c867a693992d8534d1834
sai798187 <111174945+sai798187@users.noreply.github.com>
2026-04-28 18:16:53 +0530

    Update error.js

--- controllers/error.js
+++ controllers/error.js
@@ -1,14 +1,27 @@
+const getSafeCsrfToken = req => {
+  try {
+    return req.csrfToken ? req.csrfToken() : '';
+  } catch (error) {
+    return '';
+  }
+};
+
 exports.get404 = (req, res, next) => {
   res.status(404).render('404', {
     title: 'Page Not Found',
-    path: null
+    path: null,
+    isAuthenticated: req.session ? req.session.isLoggedIn : false,
+    csrfToken: getSafeCsrfToken(req)
   });
 };
 
-exports.get500 = (req, res, next) => {
+exports.get500 = (error, req, res, next) => {
+  console.log(error);
   res.status(500).render('500', {
-    pageTitle: 'Error',
-    path: '/500',
-    isAuthenticated: false
+    title: 'Unexpected Error',
+    path: null,
+    isAuthenticated: req.session ? req.session.isLoggedIn : false,
+    csrfToken: getSafeCsrfToken(req)
   });
 };
+
