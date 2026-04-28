ff728fcd43aba3dd5dbcc369170905a2b4a83a5d
sai798187 <111174945+sai798187@users.noreply.github.com>
2026-04-28 17:55:57 +0530

    Update is-auth.js

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,11 @@
-module.exports = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
-    return res.redirect('/login');
-  }
+//module.exports = (req, res, next) => {
+ // if (!req.session.isLoggedIn) {
+ //   return res.redirect('/login');
+ // }
+ // next();
+//}
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session ? req.session.isLoggedIn : false;
+  res.locals.csrfToken = req.csrfToken ? req.csrfToken() : '';
   next();
-}
+});
